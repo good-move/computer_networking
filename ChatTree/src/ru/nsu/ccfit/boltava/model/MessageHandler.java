3b168c48cfc845ae51314625e606360e262defb3
Alexey Boltava <goodmoveab@gmail.com>
2017-10-08 20:56:21 +0700

    Modify MessageHandler to use all previously added Client features

--- ChatTree/src/ru/nsu/ccfit/boltava/model/MessageHandler.java
+++ ChatTree/src/ru/nsu/ccfit/boltava/model/MessageHandler.java
@@ -1,94 +1,43 @@
 package ru.nsu.ccfit.boltava.model;
 
 import ru.nsu.ccfit.boltava.model.message.*;
+import static ru.nsu.ccfit.boltava.model.Client.State.*;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class MessageHandler implements IMessageHandler {
 
-    private final TreeNode mTreeNode;
+    private final Client client;
 
-    MessageHandler(TreeNode treeNode) {
-        mTreeNode = treeNode;
+    MessageHandler(Client client) {
+        this.client = client;
     }
 
     @Override
     public void handle(AckMessage message) {
-        System.out.println("Received ACK serializer");
-
-        //if no matching pair found, ignore serializer
-        if (!mTreeNode.isPending(message.getId())) {
-            try {
-                mTreeNode.sendMessage(new ErrorMessage(/*errText, errCode*/), message.getSender());
-            } catch (JAXBException | IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        Message sentMessage = mTreeNode.getPending(message.getId());
-        Class sentMessageClass = sentMessage.getClass();
-
-        switch (mTreeNode.getState()) {
-            case Starting: {
-                if (sentMessageClass.equals(JoinMessage.class)) {
-                    mTreeNode.removePending(sentMessage);
-                    mTreeNode.launchChat();
-                    mTreeNode.setState(TreeNode.State.Running);
-                }
-            } break;
-            case Running: {
-                if (sentMessageClass.equals(TextMessage.class)) {
-                    mTreeNode.removePending(sentMessage);
-                }
-            } break;
-
-            case Moving: {
-                if (sentMessageClass.equals(JoinMessage.class)) {
-                    mTreeNode.removePending(sentMessage);
-//                    mTreeNode.sendMessage(new AckMessage(/* ID OF REJOIN MESSAGE */), mTreeNode.getParent());
-                    mTreeNode.confirmNextParent();
-                    mTreeNode.setState(TreeNode.State.Running);
-                }
-            } break;
-
-            case Shutting: {
-                if (sentMessageClass.equals(RejoinMessage.class)) {
-                    mTreeNode.removePending(sentMessage);
-                    try {
-                        mTreeNode.sendMessage(new AckMessage(sentMessage.getId()), sentMessage.getSender());
-                    } catch (JAXBException | IOException e) {
-                        e.printStackTrace();
-                    }
-                } else if (sentMessageClass.equals(RootMessage.class)) {
-
-                } else if (sentMessageClass.equals(LeaveMessage.class)) {
-                    mTreeNode.setState(TreeNode.State.Terminated);
-                }
-            } break;
-        }
+        System.out.println("Received ACK message");
+        // should publish ACK event
     }
 
     @Override
     public void handle(ErrorMessage message) {
-        // log serializer to stderr
+        // log error to stderr
     }
 
     @Override
     public void handle(JoinMessage message) {
-        System.out.println("Received JOIN serializer");
-
-        if (mTreeNode.getState() == TreeNode.State.Running || mTreeNode.getState() == TreeNode.State.Moving) {
-            if (!mTreeNode.isRegistered(message)) {
-                mTreeNode.register(message);
-                System.out.println(message.getNodeName() + " has joined chat");
-                mTreeNode.addChild(message.getSender());
-            }
-            System.out.println("Sending AckJoin to " + message.getNodeName());
+        System.out.println("Received JOIN message");
+        if ((client.getState() == Running || client.getState() == Moving) &&
+            !client.isMessageRegistered(message)) {
             try {
-                mTreeNode.sendMessage(new AckMessage(message.getId()), message.getSender());
-            } catch (JAXBException | IOException e) {
+                client.registerMessage(message);
+                System.out.println(message.getNodeName() + " has joined chat");
+                client.getNode().addChild(message.getSender());
+                System.out.println("Sending AckJoin to " + message.getNodeName());
+                client.sendTo(new AckMessage(message.getId()), message.getSender());
+            } catch (IOException | JAXBException | InterruptedException e) {
                 e.printStackTrace();
             }
         }
@@ -96,14 +45,14 @@ public class MessageHandler implements IMessageHandler {
 
     @Override
     public void handle(LeaveMessage message) {
-        System.out.println("Received LEAVE serializer");
+        System.out.println("Received LEAVE message");
 
-        TreeNode.State state = mTreeNode.getState();
-        if (state == TreeNode.State.Running || state == TreeNode.State.Moving) {
-            mTreeNode.removeChild(message.getSender());
+        if ((client.getState() == Running || client.getState() == Moving) &&
+            client.getNode().getChild(message.getSender()) != null) {
             try {
-                mTreeNode.sendMessage(new AckMessage(message.getId()), message.getSender());
-            } catch (JAXBException | IOException e) {
+                client.getNode().removeChild(message.getSender());
+                client.sendTo(new AckMessage(message.getId()), message.getSender());
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
@@ -111,38 +60,68 @@ public class MessageHandler implements IMessageHandler {
 
     @Override
     public void handle(RejoinMessage message) {
-        System.out.println("Received REJOIN serializer");
+        System.out.println("Received REJOIN message");
+        if(client.isRoot()) return;
 
-        if (mTreeNode.getState() == TreeNode.State.Running &&
-            message.getSender().equals(mTreeNode.getParent())) {
-            mTreeNode.setState(TreeNode.State.Moving);
-//            mTreeNode.setNextParent(message.getNextParent());
-            mTreeNode.joinNextParent();
+        InetSocketAddress parentAddress = client.getNode().getParent().getAddress();
+        if (client.getState() == Running &&
+            message.getSender().equals(parentAddress)) {
+            try {
+                client.setState(Moving);
+                client.sendTo(
+                        new JoinMessage(client.getNode().getName()),
+                        message.getNewParentAddress(),
+                        () -> {
+                            try {
+                                client.sendTo(new AckMessage(message.getId()), parentAddress);
+                                client.detachParent();
+                                client.setParent(message.getNewParentAddress());
+                                client.setState(Running);
+                            } catch (InterruptedException | JAXBException | IOException e) {
+                                e.printStackTrace();
+                                System.err.println("Failed to set new parent");
+                            }
+                        },
+                        client::detachParent
+                );
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public void handle(RootMessage message) {
-        System.out.println("Received ROOT serializer");
+        System.out.println("Received ROOT message");
 
+        if (client.getState() == Running &&
+            !client.isRoot() &&
+            message.getSender().equals(client.getNode().getParent().getAddress())) {
+            try {
+                client.sendTo(new AckMessage(message.getId()), message.getSender());
+                client.detachParent();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void handle(TextMessage message) {
-        System.out.println("Received TEXT serializer");
+        System.out.println("Received TEXT message");
 
-        if (mTreeNode.getState() == TreeNode.State.Running || mTreeNode.getState() == TreeNode.State.Moving) {
-            System.out.println("Sending AckText to " + message.getSenderName());
+        if (client.getState() == Running || client.getState() == Moving) {
             try {
-                mTreeNode.sendMessage(new AckMessage(message.getId()), message.getSender());
-            } catch (JAXBException | IOException e) {
+                System.out.println("Sending AckText to " + message.getSenderName());
+                client.sendTo(new AckMessage(message.getId()), message.getSender());
+                System.out.println("Sent AckText to " + message.getSenderName());
+                if (!client.isMessageRegistered(message)) {
+                    client.registerMessage(message);
+                    client.showMessage(message);
+                    client.broadcastMessage(message);
+                }
+            } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-            System.out.println("Sent AckText to " + message.getSenderName());
-            if (!mTreeNode.isRegistered(message)) {
-                mTreeNode.register(message);
-                mTreeNode.showMessage(message);
-                mTreeNode.broadсastMessage(message, message.getSender());
             }
         }
     }
