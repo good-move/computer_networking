80b1c0e4d6cbc3834423e264b9e74cece94cd3b5
Alexey Boltava <goodmoveab@gmail.com>
2017-11-08 23:21:40 +0700

    Make interface definition more robust to include statements

--- MD5/include/ResponseHandler.h
+++ MD5/include/ResponseHandler.h
@@ -6,17 +6,13 @@
 #define MD5_RESPONSEHANDLER_H
 
 
-#include "response/PostAnswerResponse.h"
-#include "response/ErrorResponse.h"
-#include "response/RegisterResponse.h"
-#include "response/GetRangeResponse.h"
 
 class ResponseHandler {
 public :
-  virtual void operator()(const ErrorResponse& errorResponse) = 0;
-  virtual void operator()(const RegisterResponse& errorResponse) = 0;
-  virtual void operator()(const GetRangeResponse& errorResponse) = 0;
-  virtual void operator()(const PostAnswerResponse& errorResponse) = 0;
+  virtual void operator()(const class ErrorResponse& errorResponse) = 0;
+  virtual void operator()(const class RegisterResponse& errorResponse) = 0;
+  virtual void operator()(const class GetRangeResponse& errorResponse) = 0;
+  virtual void operator()(const class PostAnswerResponse& errorResponse) = 0;
 };
 
 
