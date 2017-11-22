d630eaea183225f1a9c7f7d80b93f0fed37e7fb2
Alexey Boltava <goodmoveab@gmail.com>
2017-11-22 13:44:33 +0700

    Rename user controller to app controller. Move message controller to app controller.

--- REST_Chat/server/src/controller/app_controller.py
+++ REST_Chat/server/src/controller/app_controller.py
@@ -1,26 +1,25 @@
 import uuid
-from enum import Enum
-
 import re
 
 from pyrest.decorators import RouteController, POST, GET
 from pyrest.http import HttpRequest, HttpResponse, HttpJsonResponse, HttpJsonRequest, Headers, ContentType
+from src.model.message_storage import MessageStorage
 from src.model.user import Status, User
 from src.model.users_storage import UserStorage
 
 
-def check_auth(func):
+def authorization(func):
 
     def decorator(self, request: HttpRequest, *args, **kwargs):
         token_header = request.headers.get(Headers.auth)
         if token_header is None:
-            return HttpResponse(401)
-        if not UserController.is_auth_header_format_valid(token_header):
-            return HttpResponse(400)
+            return HttpResponse(401, 'Unauthorized')
+        if not AppController.is_auth_header_format_valid(token_header):
+            return HttpResponse(400, 'Bad Request')
 
-        token_value = UserController.parse_auth_header(token_header)
+        token_value = AppController.get_auth_header_value(token_header)
         if self.authorized_users.get(token_value) is None:
-            return HttpResponse(403)
+            return HttpResponse(403, 'Forbidden')
 
         return func(self, request, *args, **kwargs)
 
@@ -28,10 +27,11 @@ def check_auth(func):
 
 
 @RouteController
-class UserController:
+class AppController:
 
     def __init__(self):
         self.__users_db = UserStorage()
+        self.__messages_db = MessageStorage()
         self.authorized_users = dict()
 
     @POST('/login')
@@ -51,7 +51,7 @@ class UserController:
         return HttpResponse(401).add_header('WWW-Authenticate', 'Token realm=\'Username is already in use\'')
 
     @GET('/logout')
-    @check_auth
+    @authorization
     def logout(self, request: HttpRequest) -> HttpResponse:
         if not self.__check_auth(request):
             pass
@@ -59,7 +59,7 @@ class UserController:
         return HttpResponse()
 
     @GET('/users')
-    @check_auth
+    @authorization
     def get_user_list(self, request: HttpRequest) -> HttpResponse:
         users = [self.__serialize_user(user) for user in self.__users_db.get_online_users()]
         return HttpJsonResponse({
@@ -67,7 +67,7 @@ class UserController:
         })
 
     @GET('/users/{id:int}')
-    @check_auth
+    @authorization
     def get_user(self, request: HttpRequest, user_id: int) -> HttpResponse:
         print('GET ' + request.path)
 
@@ -84,11 +84,21 @@ class UserController:
             'status': repr(user.get_status())
         }
 
+    @POST('/messages')
+    @authorization
+    def post_message(self, request: HttpRequest) -> HttpResponse:
+        return HttpResponse()
+
+    @GET('/messages')
+    @authorization
+    def get_message_list(self, request: HttpRequest) -> HttpResponse:
+        return HttpResponse()
+
     @staticmethod
     def is_auth_header_format_valid(auth_header: str) -> bool:
         print(auth_header)
         return re.match(r"^Authorization: Token [-a-zA-Z0-9]+$", auth_header) is not None
 
     @staticmethod
-    def parse_auth_header(auth_header: str):
+    def get_auth_header_value(auth_header: str) -> str:
         return auth_header.split(' ')[2]
