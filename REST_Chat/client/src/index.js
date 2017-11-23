2a021a4c0ae449024e5a8095109bd7e017a639c5
Alexey Boltava <goodmoveab@gmail.com>
2017-11-23 13:57:44 +0700

    Add root js and styles files

---
+++ REST_Chat/client/src/index.js
@@ -0,0 +1,15 @@
+import styles from './styles/main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+  return (
+      <div>Hello, Darling!</div>
+  );
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
