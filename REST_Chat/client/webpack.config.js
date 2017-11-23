0e72735f2f7ec38846b0d788505f17d75807f410
Alexey Boltava <goodmoveab@gmail.com>
2017-11-23 13:16:10 +0700

    Fix client app configuration files

--- REST_Chat/client/webpack.config.js
+++ REST_Chat/client/webpack.config.js
@@ -3,21 +3,21 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: path.resolve(__dirname, "src/index.js"),
-    context: path.resolve(__dirname, "dist"),
     output: {
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
     module: {
         rules: [
             {
                 enforce: "pre",
-                test: /.(css|sass|scss)$/,
-                exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
+                test: /\.(css|sass|scss)$/,
+                exclude: path.resolve(__dirname, "node_modules"),
+                loaders: ExtractTextPlugin.extract(["css-loader", "sass-loader"])
             },
             {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
+                test: /\.jsx?$/,
+                exclude: path.resolve(__dirname, "node_modules"),
                 loader: "babel-loader"
             }
         ]
