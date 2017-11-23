85e99a6eae91821f81b02eb7fc91b2711240e07c
Alexey Boltava <goodmoveab@gmail.com>
2017-11-24 00:50:24 +0700

    Extend base App class to handle authentication and redirection in case of successful login event

--- REST_Chat/client/src/index.js
+++ REST_Chat/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import styles from './styles/main.scss';
 
@@ -9,16 +9,44 @@ import ChatPage from './pages/chat.js';
 import NotFoundPage from './pages/not_found.js';
 
 
-const App = () => {
-  return (
-      <Switch>
-          <Route exact path='/' component={LoginPage} />
-          <Route path='/login' component={LoginPage} />
-          <Route path='/chat' component={ChatPage} />
-          <Route component={NotFoundPage} />
-      </Switch>
-  );
-};
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoggedIn: false,
+            authToken: null
+        }
+    }
+
+    onAuthTokenReceived(token) {
+        console.log("Auth. token received: " + token);
+
+        this.setState({
+            isLoggedIn: true,
+            authToken: token
+        });
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path='/' render={() => (
+                        this.state.isLoggedIn ? (
+                            <Redirect to={'/chat'} />
+                        ) : (
+                            <LoginPage onTokenReceived={this.onAuthTokenReceived.bind(this)} />
+                        )
+                    )
+                } />
+                <Route path='/chat' component={ChatPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        );
+    }
+
+}
 
 
 ReactDOM.render(
