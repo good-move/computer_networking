f37c6bcff476dc92f8bc5454b2e2e1edb8fee7be
Alexey Boltava <goodmoveab@gmail.com>
2017-11-24 15:38:52 +0700

    Create basic chat page layout and import userlist view

--- REST_Chat/client/src/pages/chat.js
+++ REST_Chat/client/src/pages/chat.js
@@ -1,8 +1,50 @@
 import React from 'react';
 
+import UserList from '../components/UserList.js';
 
-const ChatPage = () => (
-    <div>Chat Page :)</div>
-);
 
-export default ChatPage;
+
+const APP_NAME = "REST CHAT";
+
+
+export default class ChatPage extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userList: []
+        };
+    }
+
+    render() {
+        return (
+            <div className={"chatPage"}>
+                <div className={"chatHeader"}>
+                    <h1>{APP_NAME}</h1>
+                    <div className={"controls"}>
+                        <input
+                            className={"logoutButton"}
+                            type={"button"}
+                            value={"Log out"}
+                        />
+                    </div>
+                </div>
+                <div className={"chatBody"}>
+                    <div className={"userListColumn"}>
+                        <UserList userList={this.state.userList} />
+                    </div>
+                    <div className={"chatViewColumn"}>
+                        <div className={"messageListView"}>
+
+                        </div>
+                        <div className={"messageInputView"}>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
