3ded18b0a655849419a8d51b14f02896a9336196
Alexey Boltava <goodmoveab@gmail.com>
2017-11-25 21:40:48 +0700

    Add timeout disconnection support on server.

--- REST_Chat/server/src/controller/app_controller.py
+++ REST_Chat/server/src/controller/app_controller.py
@@ -1,6 +1,8 @@
 import uuid
 import re
 
+import time
+
 from pyrest.decorators import RouteController, POST, GET
 from pyrest.http import HttpRequest, HttpResponse, HttpJsonResponse, HttpJsonRequest, Headers, ContentType, \
     ResponseMessages
@@ -28,17 +30,34 @@ def authorization(func):
     return decorator
 
 
+def handle_idleness(func):
+
+    def decorator(self, request: HttpRequest, *args, **kwargs):
+        auth_token = AppController.get_auth_header_value(request.headers.get(Headers.auth))
+        last_activity_time = self.last_activity_time.get(auth_token)
+        if time.time() - last_activity_time > AppController.IDLENESS_TIMEOUT:
+            self.purge_user_with_message(auth_token, Status.SLEEPING, 'went sleeping')
+            return HttpResponse(403, ResponseMessages.messages.get(403))
+
+        self.last_activity_time[auth_token] = time.time()
+        return func(self, request, *args, **kwargs)
+
+    return decorator
+
+
 @RouteController
 class AppController:
 
     DEFAULT_MESSAGE_LIST_SIZE = 10
     MAX_MESSAGE_LIST_SIZE = 10
     DEFAULT_OFFSET = -10
+    IDLENESS_TIMEOUT = 300
 
     def __init__(self):
         self.__users_db = UserStorage()
         self.__messages_db = MessageStorage()
         self.authorized_users = dict()
+        self.last_activity_time = dict()
 
     @POST('/login')
     def login(self, request: HttpJsonRequest) -> HttpResponse:
@@ -60,6 +79,7 @@ class AppController:
             self.__users_db.update_status(Status.ONLINE, user.get_id())
             user_token = str(uuid.uuid4())
             self.authorized_users[user_token] = username
+            self.last_activity_time[user_token] = time.time()
 
             self.__messages_db.add_to_history('joined chat', user.get_id(), MessageType.NOTIFICATION)
 
@@ -73,12 +93,10 @@ class AppController:
 
     @GET('/logout')
     @authorization
+    @handle_idleness
     def logout(self, request: HttpRequest) -> HttpResponse:
         auth_token = self.get_auth_header_value(request.headers.get(Headers.auth))
-        user = self.__users_db.get_by_username(self.authorized_users.get(auth_token))
-        self.__users_db.update_status(Status.OFFLINE, username=user.get_username())
-        self.__messages_db.add_to_history('left chat', user.get_id(), MessageType.NOTIFICATION)
-        del self.authorized_users[auth_token]
+        self.purge_user_with_message(auth_token, Status.OFFLINE, 'left chat')
 
         return HttpJsonResponse({
             'message': 'bye!'
@@ -86,6 +104,7 @@ class AppController:
 
     @GET('/users')
     @authorization
+    @handle_idleness
     def get_user_list(self, request: HttpRequest) -> HttpResponse:
         users = [self.__serialize_user(user) for user in self.__users_db.get_online_users()]
 
@@ -95,6 +114,7 @@ class AppController:
 
     @GET('/users/{id:int}')
     @authorization
+    @handle_idleness
     def get_user(self, request: HttpRequest, user_id: int) -> HttpResponse:
         user = self.__users_db.get_by_id(user_id)
         if user is None:
@@ -111,6 +131,7 @@ class AppController:
 
     @POST('/messages')
     @authorization
+    @handle_idleness
     def post_message(self, request: HttpJsonRequest) -> HttpResponse:
         # if content type is not a json actually
         if not re.match(ContentType.json, request.headers.get(Headers.content_type)):
@@ -133,6 +154,7 @@ class AppController:
 
     @GET('/messages')
     @authorization
+    @handle_idleness
     def get_message_list(self, request: HttpRequest) -> HttpResponse:
         list_size = self.__init_int_value(
             request.query_params.get('count', None),
@@ -177,6 +199,13 @@ class AppController:
                 return None
 
         return default_value
+
+    def purge_user_with_message(self, auth_token: str, status: Status, message: str):
+        user = self.__users_db.get_by_username(self.authorized_users.get(auth_token))
+        self.__users_db.update_status(status, user.get_id())
+        self.__messages_db.add_to_history(message, user.get_id(), MessageType.NOTIFICATION)
+        del self.authorized_users[auth_token]
+        del self.last_activity_time[auth_token]
 
     @staticmethod
     def is_auth_header_format_valid(auth_header: str) -> bool:
