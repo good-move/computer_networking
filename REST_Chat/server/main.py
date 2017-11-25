c396112c09d42c410ff9dc84f382218ec7dcac1c
Alexey Boltava <goodmoveab@gmail.com>
2017-11-25 18:46:51 +0700

    Change server address port

--- REST_Chat/server/main.py
+++ REST_Chat/server/main.py
@@ -2,7 +2,7 @@ from pyrest.app import *
 from pyrest.parser.tree_parser import TreeRouteParser
 from src.controller.app_controller import *
 
-
-app = PyRest(RouteParserClass=TreeRouteParser)
+server_address = ('', 5000)
+app = PyRest(server_address=server_address, giRouteParserClass=TreeRouteParser)
 app.run()
 
