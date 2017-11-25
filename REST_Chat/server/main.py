d88f0755c9d1012f6ba8ff25b90f33b8424481e1
Alexey Boltava <goodmoveab@gmail.com>
2017-11-25 19:21:30 +0700

    Fix incorrect parameter name in main server module

--- REST_Chat/server/main.py
+++ REST_Chat/server/main.py
@@ -3,6 +3,6 @@ from pyrest.parser.tree_parser import TreeRouteParser
 from src.controller.app_controller import *
 
 server_address = ('', 5000)
-app = PyRest(server_address=server_address, giRouteParserClass=TreeRouteParser)
+app = PyRest(server_address=server_address, RouteParserClass=TreeRouteParser)
 app.run()
 
