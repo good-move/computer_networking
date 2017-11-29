acd7ed1e3efd3e9a0ce0d9677ac0fc030867b78a
Alexey Boltava <goodmoveab@gmail.com>
2017-11-30 00:32:02 +0700

    Add Stream classes associated with TouSocket

---
+++ TcpOverUdp/src/ru/nsu/ccfit/boltava/socket/TouInputStream.java
@@ -0,0 +1,11 @@
+package ru.nsu.ccfit.boltava.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TouInputStream extends InputStream {
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+}
---
+++ TcpOverUdp/src/ru/nsu/ccfit/boltava/socket/TouOutputStream.java
@@ -0,0 +1,13 @@
+package ru.nsu.ccfit.boltava.socket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class TouOutputStream extends OutputStream {
+
+    @Override
+    public void write(int i) throws IOException {
+
+    }
+
+}
